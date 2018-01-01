028488f1a84a0331c04e7f68abd836d1e2e245e0
carsin <carsin@users.noreply.github.com>
2018-01-01 08:34:54 -1000

    sockets: init

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 app.use(express.static("public"))
-
 
 app.get("/", (req, res) => {
     res.sendFile("/views/index.html");
 });
 
-app.listen(3000, () => console.log("App listening on part 3000"));
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+server.listen(3000, () => console.log("App listening on part 3000"));
