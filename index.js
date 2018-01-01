ee2d15840bf6d0236d3ed5f18764f5a46752c4b7
carsin <carsin@users.noreply.github.com>
2018-01-01 08:36:31 -1000

    general: clean up

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.sendFile("/views/index.html");
+    res.sendFile("index.html");
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on("connection", function(socket){
+  console.log("a user connected");
 });
 
 server.listen(3000, () => console.log("App listening on part 3000"));
